4c25ac53dcf26edd3012c72bb89d9e6f3436226c
vitor <vitorpaixaoa.loubi@gmail.comm>
2020-10-23 23:47:48 -0300

    Projeto completo, com instalador

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, Tray } = require("electron");
 
+app.setAppUserModelId('com.electron-teste.app-anotacoes');
+app.setLoginItemSettings({
+    openAsHidden: true
+});
 
 function createWindow(){
     const mainWindow = new BrowserWindow({
@@ -7,9 +11,35 @@ function createWindow(){
         height: 600
     });
 
-    mainWindow.loadURL(__dirname + "./src/index.html")
-    mainWindow.on('exit', function(){
+    mainWindow.loadFile(__dirname + '/src/index.html');
+    mainWindow.on('exited', function () {
         mainWindow = null;
+    });
+
+    mainWindow.setMenu(null);
+
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: "Mostrar", click: function(){
+                mainWindow.show();
+            }  
+        },
+        {
+            label: "Fechar", click: function(){
+                app.isQuiting = true;
+                app.quit();
+            }
+        }
+    ]);
+    const tray = new Tray(__dirname + '/img/tray.png');
+    tray.setContextMenu(contextMenu);
+
+
+    mainWindow.on('close',function(e){
+        if(!app.isQuiting){
+            e.preventDefault();
+            mainWindow.hide();
+        }
     })
 
 }
